a1c6e5fff813f58a07b11822edd3acbcc0443fee
weempsss <gz0531@naver.com>
2019-11-18 10:35:16 +0900

    d

--- js/JQTrans.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    var media = window.matchMedia(mediaQueryString)
-    
-    (matchMedia("screen and (min-device-width: 740px) and (max-device-width: 1023px)") {
-        
-     }
-});
